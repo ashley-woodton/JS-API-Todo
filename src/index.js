4f0e6ba9624b7e401b08b14e5d401936cdb53d07
ashley-woodton <ashley.woodton@gmail.com>
2021-02-09 00:03:38 -0500

    mysql connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'A$h__Ca5H-1997',
+    database: 'todo'
+   });
+
+try {
+    connection.connect();
+} catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+}
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
@@ -9,9 +24,15 @@ api.listen(3000, ()=>{
     console.log('API up and running.')
 });
 
-
-api.post('/add', (req, res) =>{
+api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+    
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+     if (error) return res.json({ error: error });
+   connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+      if (error) return res.json({ error: error });
+   console.log(results);
+     });
+    });
    });
 

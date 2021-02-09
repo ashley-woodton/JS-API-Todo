11b229cf7390fff01399c89425e14d795f163d87
ashley-woodton <ashley.woodton@gmail.com>
2021-02-08 23:17:50 -0500

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, ()=>{
     console.log('API up and running.')
-})
+});
 
-api.get('/', (req, res) => {
-    res.send('Hello, world!');
+
+api.post('/add',(req, res)=>{
+    res.send('It works')
 })
 
